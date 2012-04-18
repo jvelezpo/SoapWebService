2eaa4e84474755e939a12203d3c45a696ee2ed79
jvelezpo <sebasvelez@gmail.com>
2012-04-17 23:13:02 -0500

    seguridad 10 minutos de sesion

--- WebService/src/java/security/Secure.java
+++ WebService/src/java/security/Secure.java
@@ -3,6 +3,7 @@ package security;
 import java.security.SecureRandom;
 import java.math.BigInteger;
 import java.util.ArrayList;
+import java.util.*;
 
 public class Secure {
     
@@ -16,7 +17,26 @@ public class Secure {
         String token = new BigInteger(130, random).toString(32);
         while(tokens.contains(token))
             token = new BigInteger(130, random).toString(32);
+        token += " " + new Date().getTime();
         tokens.add(token);
         return token;
     }
+    
+    /*
+     * Retorna si existe el token, el token solo dura 10 minutos para cada sesion
+     */
+    public boolean existeToken(String token){
+        long millsecPerMinute = 60 * 1000;
+        String tok[] = token.split(" ");
+        long time = Long.getLong(tok[1]);
+        
+        long diferencia = ( (new Date().getTime()) - time ) / millsecPerMinute;
+        
+        if(diferencia > 10){        //Se paso el tiempo y se elimina el token
+            tokens.remove(tokens.indexOf(token));
+        }
+        return tokens.contains(token);
+    }
+    
+    
 }
